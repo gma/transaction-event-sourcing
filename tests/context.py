53a906f9ce38510d0bfd5efe0478b269d04ffcd1
Graham Ashton <graham@effectif.com>
2024-11-14 18:56:27 +0000

    Rename mylib package to bank

--- tests/context.py
+++ tests/context.py
@@ -7,4 +7,4 @@ sys.path.insert(0, project_root)
 # To make the modules in your package available within your tests, import them
 # here:
 
-# from mylib import module  # noqa: E402
+# from bank import module  # noqa: E402
